a146cf9e8aaff3d70bf8b3fe3187e6ed083f0108
daydaylee1227 <1243437764@qq.com>
2021-03-03 23:40:29 +0800

    doc: 节流

--- 常考面试题/手写节流.js
+++ 常考面试题/手写节流.js
@@ -1,5 +1,17 @@
 // 触发实践后，每隔一段实践执行
 // 比如 滚动条事件
-const throttle = (fn, delay) => {
-
+function throttle(fn, delay) {
+    let flag = true,
+        timer = null
+    return function(...args) {
+        let context = this
+        if(!flag) return
+        
+        flag = false
+        clearTimeout(timer)
+        timer = setTimeout(function() {
+            fn.apply(context,args)
+            flag = true
+        },delay)
+    }
 }
